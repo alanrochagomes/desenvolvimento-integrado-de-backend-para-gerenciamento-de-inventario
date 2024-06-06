5048c916b3b7d93e31699143b1f8dfbf567fa3f7
Alan Rocha <141082245+alanrochagomes@users.noreply.github.com>
2024-06-06 15:32:33 +0000

    Criação inicial do Service

---
+++ src/inventario/inventario.service.js
@@ -0,0 +1,23 @@
+function readAll() {
+    return []
+}
+
+function readById() {
+}
+
+function create() {
+}
+
+function updateById() {
+}
+
+function deleteById() {
+}
+
+module.exports = {
+    readAll,
+    readById,
+    create,
+    updateById,
+    deleteById
+}
