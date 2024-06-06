aa783c86f97784c4fbf598b18381ef668f468041
Alan Rocha <141082245+alanrochagomes@users.noreply.github.com>
2024-06-06 18:08:24 +0000

    Tratamento de erros assíncronos

--- src/index.js
+++ src/index.js
@@ -19,6 +19,11 @@ async function main() {
 
   app.use("/inventario", inventarioRouter);
 
+  app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send({ error: 'Algo deu errado!'})
+  })
+
   app.listen(3000, function () {
     console.log("Servidor rodando em http://localhost:3000");
   });
