3a738f681687f3eb16a9f03d00acb539957884bb
Alan Rocha <141082245+alanrochagomes@users.noreply.github.com>
2024-06-06 12:09:53 +0000

    Endpoint Read All `[GET] /inventario` implementado

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.get('/oi', function (req, res) {
     res.send('Olá, mundo!')
 })
 
+const lista = ['Notebook', 'Phone', 'Tablet']
+
+// Endpoint Read All [GET] /inventario
+app.get('/inventario', function (req, res) {
+  res.send(lista)
+})
+
 app.listen(3000), function () {
     console.log("Aplicação rodando em http://localhost:3000")
 }
