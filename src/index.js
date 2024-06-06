9a88bb4c615e7505fd97daad49a34e1606161b6d
Alan Rocha <141082245+alanrochagomes@users.noreply.github.com>
2024-06-06 18:03:06 +0000

    CORS habilitado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const { connectToDatabase } = require("./db/database-connection");
+const cors = require('cors')
 
 const inventarioRouter = require("./inventario/inventario.router");
 
@@ -10,6 +11,7 @@ async function main() {
   const app = express();
 
   app.use(express.json());
+  app.use(cors())
 
   app.get("/", function (req, res) {
     res.send("Hello World");
